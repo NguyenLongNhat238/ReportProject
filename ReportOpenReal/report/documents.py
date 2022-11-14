c4fbd0da50db86a0ad64afe252e67c50fa8c1702
NguyenLongNhat238 <kennguyen.cr1@gmail.com>
2022-11-14 17:15:40 +0700

    Update documents.py

--- ReportOpenReal/report/documents.py
+++ ReportOpenReal/report/documents.py
@@ -8,7 +8,7 @@ from report.models import RealEstate2022, RealEstate2021
 class RealEstate2022Document(Document):
     class Index:
         name = 'real-estate-2022'
-        settings = {'number_of_shards': 12,
+        settings = {'number_of_shards': 20,
                     'number_of_replicas': 1}
 
     class Django:
@@ -19,16 +19,16 @@ class RealEstate2022Document(Document):
                   ]
 
 
-# @registry.register_document
-# class RealEstate2021Document(Document):
-#     class Index:
-#         name = 'real-estate-2021'
-#         settings = {'number_of_shards': 12,
-#                     'number_of_replicas': 1}
+@registry.register_document
+class RealEstate2021Document(Document):
+    class Index:
+        name = 'real-estate-2021'
+        settings = {'number_of_shards': 20,
+                    'number_of_replicas': 1}
 
-#     class Django:
-#         model = RealEstate2021
-#         fields = ['id_client', 'ads_link', 'ads_title', 'for_sale', 'for_lease',
-#                   'price', 'surface', 'pro_width', 'pro_length', 
-#                   'full_address','format_street','split_ward','split_district','split_city'
-#                   ]
+    class Django:
+        model = RealEstate2021
+        fields = ['id_client', 'ads_link', 'ads_title', 'for_sale', 'for_lease',
+                  'price', 'surface', 'pro_width', 'pro_length', 
+                  'full_address','format_street','split_ward','split_district','split_city'
+                  ]
