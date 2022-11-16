0001baa1ea21e99be639e32486dcf413b7b0690f
NguyenLongNhat238 <kennguyen.cr1@gmail.com>
2022-11-16 10:40:33 +0700

    Update documents.py

--- ReportOpenReal/report/documents.py
+++ ReportOpenReal/report/documents.py
@@ -7,7 +7,9 @@ from report.models import RealEstate2022, RealEstate2021
 @registry.register_document
 class RealEstate2022Document(Document):
     def get_queryset(self):
-        return self.django.model._default_manager.all()[0:1000]
+        count = RealEstate2021Document.search().count()
+        return self.django.model._default_manager.all()[count:count+1000000]
+
     class Index:
         name = 'real-estate-2022'
         settings = {'number_of_shards': 12,
@@ -15,10 +17,10 @@ class RealEstate2022Document(Document):
 
     class Django:
         model = RealEstate2022
-        fields = ['id','id_client', 'ads_link', 'ads_title', 'for_sale', 'for_lease', 'land_type',
-                  'price', 'price_m2', 'surface', 'pro_width', 'pro_length', 
-                  'full_address','format_street','split_ward','split_district','split_city',
-                  'dealer_email','dealer_tel','dealer_name'
+        fields = ['id', 'id_client', 'ads_link', 'ads_title', 'ads_date', 'for_sale', 'for_lease', 'land_type',
+                  'price', 'price_m2', 'surface', 'pro_width', 'pro_length',
+                  'full_address', 'format_street', 'split_ward', 'split_district', 'split_city',
+                  'dealer_email', 'dealer_tel', 'dealer_name'
                   ]
 
 
@@ -26,7 +28,8 @@ class RealEstate2022Document(Document):
 class RealEstate2021Document(Document):
     def get_queryset(self):
         count = RealEstate2021Document.search().count()
-        return self.django.model._default_manager.all()[count:count+1000]
+        return self.django.model._default_manager.all()[count:count+1000000]
+
     class Index:
         name = 'real-estate-2021'
         settings = {'number_of_shards': 12,
@@ -34,8 +37,8 @@ class RealEstate2021Document(Document):
 
     class Django:
         model = RealEstate2021
-        fields = ['id','id_client', 'ads_link', 'ads_title', 'for_sale', 'for_lease', 'land_type',
-                  'price', 'price_m2', 'surface', 'pro_width', 'pro_length', 
-                  'full_address','format_street','split_ward','split_district','split_city',
-                  'dealer_email','dealer_tel','dealer_name'
+        fields = ['id', 'id_client', 'ads_link', 'ads_title', 'ads_date', 'for_sale', 'for_lease', 'land_type',
+                  'price', 'price_m2', 'surface', 'pro_width', 'pro_length',
+                  'full_address', 'format_street', 'split_ward', 'split_district', 'split_city',
+                  'dealer_email', 'dealer_tel', 'dealer_name'
                   ]
