277a649f63f590e12a15a8337915c73c135cbccb
NguyenLongNhat238 <kennguyen.cr1@gmail.com>
2022-12-22 13:42:51 +0700

    Update serializers.py

--- ReportOpenReal/report/serializers.py
+++ ReportOpenReal/report/serializers.py
@@ -15,9 +15,12 @@ class RealEstate2021Serializer(serializers.ModelSerializer):
 
 class RealEstate2022Serializer(serializers.ModelSerializer):
     class Meta:
+
         model = RealEstate2022
-        fields = ['id', 'id_client', 'ads_link', 'ads_title', 'ads_date', 'for_sale', 'for_lease', 'land_type',
-                  'price', 'price_m2', 'surface', 'pro_width', 'pro_length',
-                  'full_address', 'format_street', 'split_ward', 'split_district', 'split_city',
-                  'dealer_email', 'dealer_tel', 'dealer_name'
-                  ]
+        fields = ['id_client', 'site', 'ads_link', 'for_sale', 'for_lease', 'to_buy',
+                  'to_lease', 'land_type', 'ads_date', 'price', 'price_m2', 'surface',
+                  'used_surface', 'pro_width', 'pro_length', 'legal_status', 'pro_current_status', 'pro_direction',
+                  'frontage', 'alley_access', 'pro_utilities', 'nb_rooms', 'nb_floors', 'full_address', 'format_hs', 'format_street',
+                  'split_ward', 'split_district', 'split_city', 'lat', 'lon', 'ads_title', 'detailed_brief',
+                  'dealer_name', 'dealer_address', 'dealer_email', 'dealer_type', 'dealer_tel',
+                  'project_name', 'agency_name', 'agency_address', 'agency_city', 'agency_tel', 'agency_website']
